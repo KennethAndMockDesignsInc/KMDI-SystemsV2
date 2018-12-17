983d12dfab3034fa765da2eb0989f466f5e32c17
Russell222 <26921122+Russell222@users.noreply.github.com>
2018-12-17 08:51:37 +0800

    Deleted KMDISystemsMarketing Folder

--- KMDI-Systems/KMDISystemsMarketing/Dataset/Marketing_Inventory.vb
+++
@@ -1,9 +0,0 @@
-﻿Partial Class Marketing_Inventory
-    Partial Public Class MARKETING_INVENTORY_V2DataTable
-
-    End Class
-
-    Partial Public Class Inventory_ItemDataTable
-
-    End Class
-End Class
--- KMDI-Systems/KMDISystemsMarketing/Forms/Print_QRCodes.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class Print_QRCodes
-    Private Sub Print_QRCodes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.ReportViewer1.RefreshReport()
-    End Sub
-End Class
