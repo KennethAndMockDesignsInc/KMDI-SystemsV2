f52b8527882c4c760a6b57d56e33ea9ec08c68fc
Ej1224 <39943828+Ej1224@users.noreply.github.com>
2018-12-28 08:44:14 +0800

    12.28.2018 8:43 AM
    To MERGE with MASTER

--- KMDI-Systems/My Project/Application.Designer.vb
+++ KMDI-Systems/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.KMDI_Systems.Project_Details
+            Me.MainForm = Global.KMDI_Systems.KMDISystemsLogin
         End Sub
     End Class
 End Namespace
